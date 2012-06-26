16253bf7b5e0bf146a7a0e98c5c8498c38620253
Ariel Krakowski <arikrak@email.com>
2012-06-26 12:37:04 -0400

    scafolded a depot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DepotAwdwr::Application.config.secret_token = '6fccb126c4d87ae8236fdd3601cfb2ff7e3da15668c510c5ea6ff7ed95cf35265d0f6a58774a1558ee4a3ff6c28cf597627db1be928417fb4ca9d9aa7dc59637'
